f51d34a570284dcf542c69069e3829d83f560d3b
olha1313 <oliapankiv13@gmail.com>
2020-01-22 11:26:33 +0200

    Initial comit

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('autoprefixer');
+const browserSync = require('browser-sync');
+const sourcemaps = require('gulp-sourcemaps');
+const postcss = require('gulp-postcss');
+
+gulp.task('sass', () => {
+    return gulp.src('app/scss/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass()).on('error', sass.logError)
+        .pipe(postcss([autoprefixer(['last 3 versions'])]))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({stream:true}));
+});
+
+gulp.task('watch',  () => {
+    browserSync.init({
+        server: {
+            baseDir: 'app'
+        }
+    });
+    gulp.watch('app/scss/**/*.scss', gulp.series('sass'))
+});
+
+gulp.task('js', () => {
+    return gulp.src('app/js/*.js')
+        .pipe(browserSync.stream())
+});
+
+gulp.task('default', gulp.parallel('watch', () => {
+    gulp.watch('app/*.html').on('change', browserSync.reload);
+    gulp.watch("app/js/*.js", gulp.series('js'))
+}));
